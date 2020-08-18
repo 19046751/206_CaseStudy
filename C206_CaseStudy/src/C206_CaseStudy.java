9296c0b127a03de5ea7cee70118f03dee0506dab
18016183 <18016183@PFT-18016183.rp.edu.sg>
2020-08-18 21:25:38 +0800

    Bikeparts, BikePartsDB, BikePartsTest

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,54 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
 
+	private static final int OPTION_QUIT = 5;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//insert initial data
+		BikePartsDB.populateBikePartsDB();
+		
+		int option = 0;
+		int subOption = 0;	
+		while (option != OPTION_QUIT) {
+			BikePartsDB.showBikePartsMenu();
+			C206_CaseStudy.showMainMenu();
+			option = Helper.readInt("Enter an option > ");
+			
+			if (option == 1) {
+				//Call BikeDB 
+				BikePartsDB.processOption(subOption);
+				
+				
+			}else if (option == 2) {
+				//Call BikePartDB menu
+				
+			}else if (option == 3) {
+				//Call Feedback menu
+				
+			}else if(option == 4) {
+				//Call Appointment menu
+		
+			}else if(option == 5) {
+				//quit
+				
+			}
+				
+		}
+		
+		//clean up data
+		BikePartsDB.cleanupBikePartsDB();
+	}
+	
+	public static void showMainMenu() {
+		//insert your menu here
 
 	}
 
+	public static String viewAllBikeParts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
