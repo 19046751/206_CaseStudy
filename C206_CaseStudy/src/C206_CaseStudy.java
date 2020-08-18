67b06ade1817e6c01f56d838c90751b9a2888890
19044600 <19044600@192.168.1.3>
2020-08-18 17:52:53 +0800

    //by see ming

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -2,52 +2,124 @@ import java.util.ArrayList;
 
 public class C206_CaseStudy {
 
-	private static final int OPTION_QUIT = 5;
-	
+	private static final int OPTION = 4;
+
 	public static void main(String[] args) {
-		
-		//insert initial data
-		//BikeDB.populateBikeDB();
-		
+
+		// insert initial data
+		// BikeDB.populateBikeDB();
+
 		int option = 0;
-			
-		while (option != OPTION_QUIT) {
-			
+
+		while (option != OPTION) {
+
 			C206_CaseStudy.showMainMenu();
 			option = Helper.readInt("Enter an option > ");
-			
+
 			if (option == 1) {
 				int subOption = 0;
 				CustomerDB.processOption(subOption);
-				//Call BikeDB 
-				//BikeDB.handleBikeOptions();
-				
-				
-			}else if (option == 2) {
-				//Call BikePartDB menu
-				
-			}else if (option == 3) {
-				//Call Feedback menu
+				// Call BikeDB
+				// BikeDB.handleBikeOptions();
+
+			} else if (option == 2) {
+				// Call BikePartDB menu
+
+			} else if (option == 3) {
+				// Call Feedback menu
 				int subOption = 0;
 				FeedbackDB.processOption(subOption);
-				
-			}else if(option == 4) {
-				//Call Appointment menu
-		
-			}else if(option == 5) {
-				//quit
-				
+
+			} else if (option == 4) {
+				// Call Appointment menu
+
+			} else if (option == 5) {
+				// quit
+
 			}
-				
+
 		}
-		
-		//clean up data
-		//BikeDB.cleanupBikeDB();
+
+		// clean up data
+		// BikeDB.cleanupBikeDB();
 	}
-	
+
 	public static void showMainMenu() {
 		//insert your menu here
+		// TODO Auto-generated method stub
+		int option = 0;
+		
+		while(option != OPTION) {
+			
+			AppointmentDB.showAppointmentMenu();
+			option = Helper.readInt("Enter an option > ");
+			
+			if(option == 1) {
+				Appointment.viewAppointmentList();
+			}
+			else if(option == 2) {
+				Appointment.addAppointment();
+			}
+			else if(option == 3) {
+				Appointment.delAppointment();;
+			}	
+		}
+		System.out.println("GoodBye! See You Again!");
+	}
 
+	/**
+	 * @param apptList
+	 * @param appt1
+	 */
+	public static void addAppointment(ArrayList<Appointment> apptList, Appointment appt1) {
+		// TODO Auto-generated method stub
+		String date = Helper.readString("Enter date > ");
+		String time = Helper.readString("Enter time > ");
+		String name = Helper.readString("Enter name > ");
+
+		Appointment newCust = new Appointment(date, time, name);
+		apptList.add(newCust);
+		
+		if(apptList.size() != 0) {
+			System.out.println("Successfully added!");
+		}
+		else {
+			System.out.println("Add fail");
+		}
+	}
+
+	public static Boolean delAppointment(ArrayList<Appointment> apptList, Appointment appt1) {
+		// TODO Auto-generated method stub
+		Appointment.viewAppointmentList();
+		int num = Helper.readInt("Enter NO. to delete > ");
+		boolean isDeleted = false;
+		String output = "";
+		
+		if (isDeleted == false) {
+			apptList.remove(num-1);
+			isDeleted = true;
+			System.out.println("Successfully deleted!");
+		} else {
+			System.out.println("Delete unsuccessful.");
+		}
+		return null;
+	}
+
+	public static String viewAppointmentList(ArrayList<Appointment> apptList) {
+		// TODO Auto-generated method stub
+		if (apptList.size() == 0) {
+			System.out.println("No customers.");
+		}
+		else {
+			// display all Customers in the list
+			String output = String.format("%-10s %-10s %-10s %-10s\n", "NO.", "NAME", "DATE", "TIME");
+			for (int i = 0; apptList.size() > i; i++) {
+				output += String.format("%-10d %-10s %-10s %-10s\n", i + 1, apptList.get(i).getCustomerName(),
+						apptList.get(i).getDate(), apptList.get(i).getTime());
+			}
+			System.out.println(output);
+		}
+		return null;
+	}
 	}
 	
-}
---
+++ C206_CaseStudy/test/AppointmentDBTest.java
@@ -0,0 +1,94 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 19044600, Aug 11, 2020 4:09:25 PM
+ */
+
+/**
+ * @author 19044600
+ *
+ */
+public class AppointmentDBTest {
+	private Appointment appt1;
+	private Appointment appt2;
+
+	private ArrayList<Appointment> apptList;
+
+	@Before
+	public void setUp() throws Exception {
+		apptList = new ArrayList<Appointment>();
+		appt1 = new Appointment("20-5-2020", "14:00", "John");
+		appt2 = new Appointment("21-5-2020", "11:00", "Lisa");
+
+		apptList = new ArrayList<Appointment>();
+	}
+	@Test
+	public void viewtest() {
+		// Item list is not null, so that can display appointment list
+		assertNotNull("Test if there is valid Appointment arraylist to delete to", apptList);
+
+		// Given an empty list, test the appointment list have the size of 0
+		assertEquals("Test if that Appointment arraylist size is 0", 0, apptList.size());
+
+		// Given an empty list, after adding 2 item, the size of the list is 2
+		C206_CaseStudy.addAppointment(apptList, appt1);
+		C206_CaseStudy.addAppointment(apptList, appt2);
+		assertEquals("Test if that Appointment arraylist size is 2?", 2, apptList.size());
+
+		// Test if the expected output string same as the list of AppointmentDB
+		String viewAll = C206_CaseStudy.viewAppointmentList(apptList);
+
+		String output = String.format("%-10s %-10s %-10s %-10s\n", "NO.", "NAME", "DATE", "TIME");
+		for (int i = 0; apptList.size() > i; i++) {
+			output += String.format("%-10d %-10s %-10s %-10s\n", i + 1, apptList.get(i).getCustomerName(),
+					apptList.get(i).getDate(), apptList.get(i).getTime());
+		}
+		assertEquals("Check view appointment list", output, viewAll);
+	}
+
+	@Test
+	public void addAppointmenttest() {
+
+		// Item list is not null, so that can add a new appointment
+		assertNotNull("Test if there is valid Appointment arraylist to add to", apptList);
+
+		// Given an empty list, after adding 1 item, the size of the list is 1
+		C206_CaseStudy.addAppointment(apptList, appt1);
+		assertEquals("Test if that Appointment arraylist size is 1?", 1, apptList.size());
+
+		// The item just added is as same as the first item of the list
+		assertSame("Test that Appointment is added same as 1st item of the list?", appt1, apptList.get(0));
+
+		// Add another item. test The size of the list is 2?
+		C206_CaseStudy.addAppointment(apptList, appt2);
+		assertEquals("Test that Camcorder arraylist size is 2?", 2, apptList.size());
+
+	}
+
+	@Test
+	public void deletetest() {
+		 //Item list is not null, so that can delete a new appointment
+		assertNotNull("Test if there is valid Appointment arraylist to delete to", apptList);
+
+		// Test if the appointment able to delete
+		C206_CaseStudy.addAppointment(apptList, appt1);
+		Boolean isDeleted = C206_CaseStudy.delAppointment(apptList, appt1);
+		assertFalse("Test if appointment is deleted", isDeleted);
+
+		 //Test if the ArrayList is updated after delete
+		//isDeleted = C206_CaseStudy.delAppointment(apptList, appt1);
+		//assert
+
+	}
+
+}
