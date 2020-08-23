48a219c84f626689c6d57bebe0d7610d15a5b055
19007283 <19007283@Kai>
2020-08-23 18:41:34 +0800

    consolidate

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 public class C206_CaseStudy {
 	private static final int OPTION = 5;
 
+	// consolidate all the shit together
+	// done by boon kai!
 	public static void main(String[] args) {
 		// insert your menu here
 		// TODO Auto-generated method stub
@@ -12,31 +14,31 @@ public class C206_CaseStudy {
 		while (option != OPTION) {
 			if (option == 1) { //bikeparts
 				int subOption = 0;
-				BikePartsDB.processOption(subOption);        
+				CustomerDB.processOption(subOption);        
 			}
 
 		}
 	}
 
-	public static void addBike(ArrayList<BikeParts> bikepartsList, BikeParts b1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public static void addBikePartstest(ArrayList<BikeParts> bikepartsList, BikeParts b1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public static String viewAllBikeParts(ArrayList<BikeParts> bikepartsList) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public static void deletetest(ArrayList<BikeParts> bikepartsList, BikeParts b1) {
-		// TODO Auto-generated method stub
-		
-	}
+//	public static void addBike(ArrayList<BikeParts> bikepartsList, BikeParts b1) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	public static void addBikePartstest(ArrayList<BikeParts> bikepartsList, BikeParts b1) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	public static String viewAllBikeParts(ArrayList<BikeParts> bikepartsList) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public static void deletetest(ArrayList<BikeParts> bikepartsList, BikeParts b1) {
+//		// TODO Auto-generated method stub
+//		
+//	}
 
 
 
